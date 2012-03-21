600ebae6fcfbd06121db86e69eed6018c6eb28e3
Nicholas Letourneau <nick@pathridden.com>
2012-03-20 22:13:58 -0700

    a little code cleanup and added duration/album to the growl display

--- js/background.js
+++ js/background.js
@@ -8,12 +8,18 @@
 chrome.extension.onConnect.addListener(port_on_connect);
 
 function port_on_connect(port) {
-    console.assert(port.name == "growltunes");
+  console.assert(port.name == "growltunes");
 
-    port.onMessage.addListener(port_on_message);
+  port.onMessage.addListener(port_on_message);
 }
 
 function port_on_message(song)
 {
-    growl.notify(song.title, song.artist, "http:" + song.albumArtUrl);
+  var minutes = Math.floor(song.durationMillis/60000);
+  var seconds = Math.floor(song.durationMillis/1000 % 60)+"";
+  if (seconds.length == 1)
+    seconds = "0" + seconds;
+
+  var duration = "0:00/" + minutes + ":" + seconds;
+  growl.notify(song.title, duration + "\n" + song.artist + "\n" + song.album, "http:" + song.albumArtUrl);
 }
--- js/player.js
+++ js/player.js
@@ -10,20 +10,23 @@ var port = chrome.extension.connect({name: "growltunes"});
 var passthrough = document.createElement("div");
 passthrough.setAttribute("style", "display: none;");
 passthrough.id = "cpgPassthrough";
-document.body.appendChild(passthrough);
 
 passthrough.addEventListener('displayCPGNotification', function() {
-  var eventData = JSON.parse(document.getElementById('cpgPassthrough').innerText);
+  var eventData = JSON.parse(document.getElementById('cpgPassthrough').getAttribute('data'));
   port.postMessage(eventData);
 });
 
-var bindCommunication = function() { window.SJBaddListener('playSong', function(obj) {
-  var customEvent = document.createEvent('Event');
-  customEvent.initEvent('displayCPGNotification', true, true);
+document.body.appendChild(passthrough);
 
-  hiddenDiv = document.getElementById('cpgPassthrough');
-  hiddenDiv.innerText = JSON.stringify(obj.payload.song)
-  hiddenDiv.dispatchEvent(customEvent);
-})};
+var bindCommunication = function() {
+  SJBaddListener('playSong', function(obj) {
+    var customEvent = document.createEvent('Event');
+    customEvent.initEvent('displayCPGNotification', true, true);
+
+    var passthrough = document.getElementById('cpgPassthrough');
+    passthrough.setAttribute('data', JSON.stringify(obj.payload.song));
+    passthrough.dispatchEvent(customEvent);
+  })
+};
 
 injectScript(bindCommunication);
