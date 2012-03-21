67628afaeb506c29b7b39e3ff6685e1d7c836293
Nick Letourneau <nickl@trulia.com>
2012-03-20 18:02:30 -0700

    removed the polling of dom elements and replaced it with adding to the built in google play javascript callbacks.

--- js/background.js
+++ js/background.js
@@ -1,28 +1,19 @@
 /**
  * background.js
  * Background page script
- * Copyright (c) 2011 Alexey Savartsov <asavartsov@gmail.com>
- * Modified by Nicholas Letourneau (http://nicholasletourneau.com) for Growl notifications
- * Licensed under the MIT license
+ * Copyright (c) 2011 2012 Nicholas Letourneau (http://nicholasletourneau.com)
+ * Licensed under the MIT license http://www.opensource.org/licenses/mit-license.php
  */
 
-// Connect event handlers
 chrome.extension.onConnect.addListener(port_on_connect);
 
-/**
- * Content script has connected to the extension
- */
 function port_on_connect(port) {
-    console.assert(port.name == "growltunes"); 
+    console.assert(port.name == "growltunes");
 
-    // Connect another port event handlers
     port.onMessage.addListener(port_on_message);
 }
- 
- /**
-  * New message arrives to the port
-  */
-function port_on_message(player)
+
+function port_on_message(song)
 {
-    growl.notify(player.song.title, player.song.artist, player.song.cover);
+    growl.notify(song.title, song.artist, "http:" + song.albumArtUrl);
 }
--- js/player.js
+++ js/player.js
@@ -1,44 +1,29 @@
 /**
- * player.js (originally: contentscripts.js)
- * Parses player page and transmit song information to background page
- * Copyright (c) 2011 Alexey Savartsov, <asavartsov@gmail.com>, Brad Lambeth <brad@lambeth.us>
- * Modified by Nicholas Letourneau (http://nicholasletourneau.com) for Growl notifications
- * Licensed under the MIT license
+ * player.js
+ * Injects JS into the google play page in order to call SJBaddListener, which adds a callback when songs change.
+ * Copyright (c) 2011 2012 Nicholas Letourneau (http://nicholasletourneau.com)
+ * Licensed under the MIT license http://www.opensource.org/licenses/mit-license.php
  */
-
-/**
- * Player class
- *
- * Cloud Player page parser
- */
-function Player(parser) {	
-    this.has_song = parser._get_has_song();
-    this.is_playing = parser._get_is_playing();
-    this.song = {
-        position: parser._get_song_position(),
-        time: parser._get_song_time(),
-        display_time: parser._get_song_display_time(),
-        title: parser._get_song_title(),
-        artist: parser._get_song_artist(),
-        album: parser._get_song_album(),
-        cover: parser._get_song_cover()
-    };
-}
 
 var port = chrome.extension.connect({name: "growltunes"});
-var player = new Player(new GoogleMusicParser());
 
-window.setInterval(
-  function()
-  {
-    var newplayer = new Player(new GoogleMusicParser());
-    if (newplayer.has_song && newplayer.is_playing &&
-      (player.song.title != newplayer.song.title || player.song.artist != newplayer.song.artist))
-    {
-      port.postMessage(newplayer);
-      player = newplayer;
-    }
-     
-  }, 
-  250
-);	
+var passthrough = document.createElement("div");
+passthrough.setAttribute("style", "display: none;");
+passthrough.id = "cpgPassthrough";
+document.body.appendChild(passthrough);
+
+passthrough.addEventListener('displayCPGNotification', function() {
+  var eventData = JSON.parse(document.getElementById('cpgPassthrough').innerText);
+  port.postMessage(eventData);
+});
+
+var bindCommunication = function() { window.SJBaddListener('playSong', function(obj) {
+  var customEvent = document.createEvent('Event');
+  customEvent.initEvent('displayCPGNotification', true, true);
+
+  hiddenDiv = document.getElementById('cpgPassthrough');
+  hiddenDiv.innerText = JSON.stringify(obj.payload.song)
+  hiddenDiv.dispatchEvent(customEvent);
+})};
+
+injectScript(bindCommunication);
